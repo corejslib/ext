76d374760fd487338408d097d0afd8fae29b5e4b
zdm <zdm@softvisio.net>
2026-09-19 10:43:00 +0300

    fix: update lint options

--- build/bin/build.js
+++ build/bin/build.js
@@ -174,7 +174,7 @@ await fs.promises.cp( `${ tmpDir.path }/node_modules/@sencha/ext-web-components-
 {
     console.log( "Compress css files" );
 
-    childProcess.spawnSync( "npx zcli lint --action=compress --no-lintignore **/*.css", {
+    childProcess.spawnSync( "npx zcli lint --action=compress **/*.css", {
         "cwd": dataDir,
         "shell": true,
         "stdio": "inherit",
@@ -183,7 +183,7 @@ await fs.promises.cp( `${ tmpDir.path }/node_modules/@sencha/ext-web-components-
     console.log();
     console.log( "Lint files" );
 
-    childProcess.spawnSync( "npx zcli lint --no-lintignore --no-log --no-eslint", {
+    childProcess.spawnSync( "npx zcli lint --no-lintignore --no-log --no-eslint-code", {
         "cwd": dataDir,
         "shell": true,
         "stdio": "inherit",
